95feab7b72d9f90278834c9e648fbc358f78e57f
deepak negi <dpk.negircks@gmail.com>
2023-04-04 23:55:57 +0100

    second commit to the local repository

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,5 +1,14 @@
 import React from "react";
 
 export default function Home() {
-  return <h1>welcome</h1>;
+  return (
+    <main className="home--main">
+      <h1>You got travel plans, we got travel vans</h1>
+      <p>
+        Add adventure to your life by joining the #vanlife movement. Rent the
+        perfect van to make your perfect road trip.
+      </p>
+      <button>Find your vans</button>
+    </main>
+  );
 }
